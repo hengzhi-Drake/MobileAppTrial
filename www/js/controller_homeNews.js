55954aa0b2e70263f68b8fa0d94e7a901a6f0ae3
zheng <zheng@J46X622.apac.drakeintl.com>
2015-06-29 16:46:35 +1000

    2015-06-29

--- www/js/controller_homeNews.js
+++ www/js/controller_homeNews.js
@@ -9,32 +9,69 @@ angular.module('starter.controllers', [])
 .controller('homeNewsCtrl', function ($scope,$rootScope,$state,$ionicLoading,searchClasses) {
       
      $scope.goSearch = function () {
-         
-         /*
-             var myAccountInfo={};
-              X2Test.test(myAccountInfo,function(result){
-                  alert('Done');
-                  alert(result.Msg);
-                  console.log(result);
-              })
-           */    
-               
-             $rootScope.messgeNumber = 0;
-               
-               
-        
-        $state.go('tab.search');
+            $rootScope.messgeNumber = 0;
+            $state.go('tab.search');
     };
     
-    $ionicLoading.show({
-      template: 'Loading...'
-    });
-        
-    setTimeout(function () {$ionicLoading.show({template: 'Get position...'});}, 5000);
-      
-    setTimeout(function () {$ionicLoading.hide();}, 10000);
+   // $ionicLoading.show({      template: 'Loading...'    });
     
-    $scope.searchClasses = searchClasses.all();
+   
+    var objthis = this;
+    
+    //var locationService = $cordovaGeolocation;
+    
+    var locationService = navigator.geolocation; // cordova geolocation plugin
+    
+    if($.isEmptyNull(locationService))
+    {
+        alert('plugin not working');        
+    }
+    
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+    
+    
+    
+    
+    setTimeout(function () {
+    locationService.getCurrentPosition({enableHighAccuracy: true, timeout: 15000}
+    ).then(function (pos)
+    {
+            var myLatlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                var mapOptions = {
+                    center: myLatlng,
+                    zoom: 8,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                
+                var domObj =$(".hbx2Mymap")[0];
+                if($.isEmptyNull(domObj) || $.isEmptyNull(domObj)[0])
+                {
+                    console.log(domObj);
+                    alert("dom empty");        
+                    return;
+                }
+               // document.getElementById("map")
+                var map = new google.maps.Map(domObj,
+                    mapOptions);
+                map.setCenter(myLatlng);
+               
+                $scope.map = map;
+           
+        
+    });
+    
+    },3000);
+    
+        
+   // setTimeout(function () {$ionicLoading.show({template: 'Get position...'});}, 5000);
+      
+   // setTimeout(function () {$ionicLoading.hide();}, 10000);
+   
+   // $scope.searchClasses = searchClasses.all();
    
    
 })
---
+++ www/myLib/x2lib.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function($) {
+    $.isEmptyNull = function(str) {
+        if ($.IsBoolean(str)) {
+            return false
+        }
+        if (str == null) {
+            return true
+        }
+        if (typeof str == "undefined") {
+            return true
+        }
+        if (str.length == 0) {
+            return true
+        }
+        if (str == "") {
+            return true
+        }
+        return false
+    };
+    
+        $.IsBoolean = function(input) {
+        var typeName = typeof(input);
+        return typeName == "boolean"
+    };
+})(jQuery);
